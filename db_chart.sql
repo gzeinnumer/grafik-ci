0685f05fe18609508a16b1a6c4ba1272b8090302
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-02-14 10:15:19 +0700

    connect

---
+++ db_chart.sql
@@ -0,0 +1,82 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 14 Feb 2020 pada 04.14
+-- Versi server: 10.3.16-MariaDB
+-- Versi PHP: 7.3.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_chart`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `barang_id` int(11) NOT NULL,
+  `nama_barang` varchar(100) DEFAULT NULL,
+  `merk` varchar(30) DEFAULT NULL,
+  `stok` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `barang`
+--
+
+INSERT INTO `barang` (`barang_id`, `nama_barang`, `merk`, `stok`) VALUES
+(1, 'Aqua 1', 'Aqua', 20),
+(2, 'Aqua 2', 'Aqua', 10),
+(3, 'Aqua 3', 'Aqua', 30),
+(4, 'Aqua 4', 'Aqua', 40),
+(5, 'Aqua 5', 'Aqua', 5),
+(6, 'Wardah 1', 'Wardah', 10),
+(7, 'Wardah 2', 'Wardah', 20),
+(8, 'Wardah 3', 'Wardah', 10),
+(9, 'Wardah 4', 'Wardah', 50),
+(10, 'Inez 1', 'Inez', 10),
+(11, 'Inez 2', 'Inez', 30),
+(12, 'VIva 1', 'Viva', 30),
+(13, 'Coca-Cola 1', 'Coca-cola', 10),
+(14, 'Coca-Cola 2', 'Coca-cola', 10),
+(15, 'Coca-Cola 3', 'Coca-cola', 30);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`barang_id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `barang_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
